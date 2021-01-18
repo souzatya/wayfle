58903bcc9ef7516aeadf39dbe5e5c60be8687cdc
Soujatya Sarkar <44564450+souzatya@users.noreply.github.com>
2021-01-18 18:44:45 +0530

    alpha_designs_first stage

---
+++ beta/lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeBrightness,
+        child: const Icon(Icons.message),
+        backgroundColor: Colors.teal[400],
+      ),
+    );
+  }
+
+  void changeBrightness() {
+    DynamicTheme.of(context).setBrightness(
+        Theme.of(context).brightness == Brightness.dark
+            ? Brightness.light
+            : Brightness.dark);
+  }
+}
