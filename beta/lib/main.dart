edb86759586772f7c8dff1e16c312ee43c0def22
Soujatya Sarkar <44564450+souzatya@users.noreply.github.com>
2021-01-20 18:46:39 +0530

    project reset to v0.0.0.a

--- beta/lib/main.dart
+++ beta/lib/main.dart
@@ -1,26 +1,5 @@
-import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
 
 void main() {
   runApp(WayfleB());
-}
-
-class WayfleB extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DynamicTheme(
-        defaultBrightness: Brightness.light,
-        data: (brightness) => ThemeData(
-              primarySwatch: Colors.green,
-              brightness: brightness,
-            ),
-        themedWidgetBuilder: (context, theme) {
-          return MaterialApp(
-            title: 'Wayfle Beta',
-            theme: theme,
-            home: Home(title: 'Wayfle Home'),
-          );
-        });
-  }
 }
