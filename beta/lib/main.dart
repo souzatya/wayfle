49b4b17ca8710a3d4316ae1d0ab8d847d8ad0d29
Soujatya Sarkar <44564450+souzatya@users.noreply.github.com>
2021-01-21 16:57:23 +0530

    v0.0.0.a #1

--- beta/lib/main.dart
+++ beta/lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(WayfleB());
+}
+
+class WayfleB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'wayfle',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        backgroundColor: Colors.white,
+        primaryColor: Colors.white,
+        fontFamily: 'Proxima Nova',
+      ),
+      home: Home(),
+    );
+  }
 }
