d704992e1ad2a64de4352cf238bcaa397c36e62f
Soujatya Sarkar <44564450+souzatya@users.noreply.github.com>
2021-01-27 05:40:51 +0530

    v0.0.0.a+1

--- beta/lib/splash.dart
+++ beta/lib/splash.dart
@@ -14,7 +14,7 @@ class _SplashState extends State<Splash> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 2),
+        Duration(seconds: 3),
         () => Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => Home())));
   }
